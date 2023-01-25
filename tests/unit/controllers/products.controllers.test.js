b476313ccf6afb20db12f62edbfe6f37c9962678
elderkampbell <elderkampbell@hotmail.com>
2023-01-25 20:17:57 -0300

    feat: second requirement

--- tests/unit/controllers/products.controllers.test.js
+++ tests/unit/controllers/products.controllers.test.js
@@ -1,10 +1,13 @@
 const { expect } = require("chai");
+const chai = require("chai");
 const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
 const { productsController } = require('../../../src/controllers');
 
 const connection = require("../../../src/models/connection");
 const { productsService } = require("../../../src/services");
-const { products } = require('../mocks/products.model.mock')
+const { products } = require('../mocks/products.model.mock');
+chai.use(sinonChai)
 
 describe('Testes controllers', function () {
   const req = {};
@@ -18,11 +21,25 @@ describe('Testes controllers', function () {
   afterEach(sinon.restore);
 
   it('All', async function () {
-    sinon.stub(productsService, 'listAllProducts').resolves(products);
+    sinon.stub(productsService, 'listAllProducts').resolves({ type: null, message: products });
     await productsController.listAllProducts(req, res);
-    expect(res.status).to.be.deep.equal(200);
-    expect(res.json).to.be.deep.equal(products)
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWithExactly(products)
   });
 
+  it('ID', async function () {
+    req.params = { id: 1 };
+    sinon.stub(productsService, 'listProductsById').resolves({ type: null, message: products[0] });
+    await productsController.listProductsById(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWithExactly(products[0])
+  });
 
+  it('Missing', async function () {
+    req.params = { id: 99 };
+    sinon.stub(productsService, 'listProductsById').resolves({ type: 'NOT_FOUND', message: 'Product not found' });
+    await productsController.listProductsById(req, res);
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWithExactly({ message: 'Product not found' })
+  });
 });
--- tests/unit/services/products.service.test.js
+++ tests/unit/services/products.service.test.js
@@ -11,18 +11,18 @@ describe('Testes service', function () {
   it('All', async function () {
     sinon.stub(connection, 'execute').resolves([products]);
     const result = await productsService.listAllProducts();
-    expect(result).to.be.deep.equal(products);
+    expect(result.message).to.be.deep.equal(products);
   });
 
   it('ID', async function () {
     sinon.stub(connection, 'execute').resolves([[products[0]]]);
     const result = await productsService.listProductsById(1);
-    expect(result).to.be.deep.equal(products[0]);
+    expect(result.message).to.be.deep.equal(products[0]);
   });
 
   it('Missing', async function () {
     sinon.stub(connection, 'execute').resolves([[products[99]]]);
     const result = await productsService.listProductsById(99);
-    expect(result).to.be.deep.equal({message: "Product not found"})
+    expect(result.message).to.be.deep.equal("Product not found")
   });
 });
